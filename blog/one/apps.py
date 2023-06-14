2f55ae4c065860cca55209f75ff11b6dfe44ceb2
@sandeepchinnam <chinnamsandeep30@gmail.com>
2023-06-14 11:31:11 +0530

    first commit

---
+++ blog/one/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+class OneConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'one'
---
+++ blog/one/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Post
+def home(request):
+    context={
+        'posts': Post.objects.all()
+    }
+    return render(request,'one/home.html',context)
+def about(request):
+    return render(request,'one/about.html',{'title': 'About'})
